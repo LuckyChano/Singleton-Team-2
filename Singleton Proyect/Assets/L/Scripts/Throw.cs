2733729814772773092de557106dbe856049c3ac
LuckyChano <101567723+LuckyChano@users.noreply.github.com>
2022-10-03 23:49:05 -0300

    Update v0.1 - Boomerang

--- Singleton Proyect/Assets/L/Scripts/Throw.cs
+++ Singleton Proyect/Assets/L/Scripts/Throw.cs
@@ -6,8 +6,9 @@ public class Throw : MonoBehaviour
 {
     private Rigidbody _weaponRb;
     private Weapon _weaponSc;
-    
+
     private float _returnTime;
+
     private Vector3 _origLocPos;
     private Vector3 _origLocRot;
     private Vector3 _pullPosition;
@@ -38,9 +39,36 @@ public class Throw : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        //Aiming
+
+        //Walking?
+
+        if (hasWeapon)
         {
-            AxeThrow();
+            if (Input.GetButtonDown("Fire1"))
+            {
+                AxeThrow();
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                WeaponStartPull();
+            }
+        }
+
+        if (pulling)
+        {
+            if (_returnTime < 1)
+            {
+                weapon.position = GetQuadraticCurvePoint(_returnTime, _pullPosition, curvePoint.position, hand.position);
+                _returnTime += Time.deltaTime * 1.5f;
+            }
+            else
+            {
+                WeaponCatch();
+            }
         }
     }
 
@@ -49,15 +77,45 @@ public class Throw : MonoBehaviour
         Debug.Log("AGARRA");
 
         hasWeapon = false;
-        //weaponSc.activated = true;
+        _weaponSc.activated = true;
         _weaponRb.isKinematic = false;
         _weaponRb.collisionDetectionMode = CollisionDetectionMode.Continuous;
         weapon.parent = null;
         weapon.eulerAngles = new Vector3(0, -90 + transform.eulerAngles.y, 0);
         weapon.transform.position += transform.right / 5;
         _weaponRb.AddForce(Camera.main.transform.forward * throwPower + transform.up * 2, ForceMode.Impulse);
-
-
     }
 
+    //AxeChatch
+    public void WeaponStartPull()
+    {
+        pulling = true;
+        _pullPosition = weapon.position;
+        _weaponRb.Sleep();
+        _weaponRb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        _weaponRb.isKinematic = true;
+        weapon.eulerAngles = new Vector3(-110, 0, 0);
+        _weaponRb.velocity = Vector3.zero;
+        _weaponSc.activated = true;
+    }
+
+    //AxeReset
+    public void WeaponCatch()
+    {
+        _returnTime = 0;
+        pulling = false;
+        weapon.parent = hand;
+        _weaponSc.activated = false;
+        weapon.localEulerAngles = _origLocRot;
+        weapon.localPosition = _origLocPos;
+        hasWeapon = true;
+    }
+
+    private Vector3 GetQuadraticCurvePoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        return (uu * p0) + (2 * u * t * p1) + (tt * p2);
+    }
 }
--- Singleton Proyect/Assets/L/Scripts/Weapon.cs
+++ Singleton Proyect/Assets/L/Scripts/Weapon.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Parameters")]
+    public float rotationSpeed;
+
+    //[HideInInspector]
+    [Header("Bools")]
+    public bool activated;
+
+    private void Update()
     {
-        
+        if (activated)
+        {
+            transform.localEulerAngles += Vector3.forward * rotationSpeed * Time.deltaTime;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log(collision.gameObject.name);
+        //GetComponent<Rigidbody>().Sleep();
+        //GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        //GetComponent<Rigidbody>().isKinematic = false;
+        activated = false;
     }
 }
