8c1bb3fcbed5b28febb0e93f78230712bbaf448a
JANONIMO <JulianDiegoBenitez@gmail.com>
2022-12-06 21:58:45 -0300

    Changes

---
+++ Singleton Proyect/Assets/Scripts/Store.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Store : MonoBehaviour
+{
+    //[SerializeField] ShopButton itemButton;
+    //[SerializeField] Transform parent;
+
+    //[SerializeField] ItemScriptable[] items = new ItemScriptable[0];
+
+    //private void Start()
+    //{
+    //    for (int i = 0; i < items.Length; i++)
+    //    {
+    //        var newButton = Instantiate(itemButton, parent);
+    //        newButton.SetButton(items[i]);
+    //    }
+    //}
+}
