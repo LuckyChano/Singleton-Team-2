3d431b1811bd5c6782960fa1b9c0235f6de81fa0
FranSerra <francisco.serra@davinci.edu.ar>
2022-12-12 21:43:07 -0300

    dada

--- Singleton Proyect/Assets/Scripts/FlyweightPointer.cs
+++ Singleton Proyect/Assets/Scripts/FlyweightPointer.cs
@@ -7,7 +7,7 @@ public static class FlyweightPointer
     public static readonly FlyweightEnemy orc = new FlyweightEnemy
     {
         maxLife = 200,
-        speed = 100,
+        speed = 10,
         moneyGain = 25
     };
 
